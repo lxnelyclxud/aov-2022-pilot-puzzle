15882ac47fff102ab5a74796728cf31a36d5ee96
lxnelyclxud <52774564+lxnelyclxud@users.noreply.github.com>
2022-11-15 22:16:21 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# aov-2022-pilot-puzzle
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/aov-2022-pilot-puzzle-mzsjfw)
---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+//
+
+const board = [
+  [null, ],
+  [],
+  [],
+]
+</script>
+
+<template>
+  <div class="w-full h-full flex justify-center items-center">
+    <div class="grid grid-rows-3 grid-cols-3">
+    
+    </div>
+  </div>
+</template>
