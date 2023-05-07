8b193a168184faf987e60b14c6e4845992b12817
Alexander B <mail@xanderbarkhatov.com>
2023-05-08 03:51:45 +0600

    add animations

--- src/components/GameBoardCell.vue
+++ src/components/GameBoardCell.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import type { Cell } from '../types'
+import { Motion } from 'motion/vue'
+import { Player, type Cell } from '@/types'
 
 type Props = {
   value: Cell
@@ -8,19 +9,66 @@ type Props = {
 }
 
 defineProps<Props>()
+
+const motionProps = {
+  pathLength: '1',
+  initial: {
+    visibility: 'hidden',
+    strokeDasharray: 1,
+    strokeDashoffset: 1
+  },
+  animate: {
+    visibility: 'visible',
+    strokeDashoffset: 0
+  },
+  exit: {
+    opacity: 0
+  }
+}
 </script>
 
 <template>
-  <button
-    :disabled="disabled"
-    :class="[value?.toLowerCase(), highlighted && 'bg-green/10']"
-    class="cell"
-  />
+  <button :disabled="disabled || !!value" :class="[highlighted && 'bg-green/10']" class="cell">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="100%"
+      height="100%"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    >
+      <Transition leave-active-class="opacity-0 transition duration-300">
+        <g v-if="value === Player.X">
+          <Motion v-bind="motionProps" :transition="{ duration: 0.5 }" tag="path" d="M18 6L6 18" />
+          <Motion
+            v-bind="motionProps"
+            :transition="{ duration: 0.5, delay: 0.5 }"
+            tag="path"
+            d="M6 6l12 12"
+          />
+        </g>
+
+        <Motion
+          v-else-if="value === Player.O"
+          v-bind="motionProps"
+          :transition="{ duration: 1 }"
+          tag="circle"
+          cx="12"
+          cy="12"
+          r="7"
+          class="-rotate-90 origin-center"
+        />
+      </Transition>
+    </svg>
+  </button>
 </template>
 
 <style scoped>
 .cell {
-  @apply p-3 text-green cursor-crosshair transition duration-100 disabled:cursor-not-allowed;
+  @apply p-3 text-green cursor-crosshair disabled:cursor-not-allowed;
 }
 
 .cell:nth-child(3n + 2) {
@@ -29,33 +77,5 @@ defineProps<Props>()
 
 .cell:nth-child(n + 4):nth-child(-n + 6) {
   @apply border-y-2;
-}
-
-.x,
-.o {
-  @apply cursor-not-allowed;
-}
-
-.x {
-  @apply relative;
-}
-
-.x::before,
-.x::after {
-  content: '';
-  @apply absolute top-0 h-full w-1 bg-current;
-}
-
-.x::before {
-  @apply rotate-45;
-}
-
-.x::after {
-  @apply -rotate-45;
-}
-
-.o::before {
-  content: '';
-  @apply border-current border-4 w-full h-full block rounded-full;
 }
 </style>
