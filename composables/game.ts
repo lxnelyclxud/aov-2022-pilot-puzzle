586e21d32cb217e26bd85e083731724ad519c39a
Alexander B <mail@xanderbarkhatov.com>
2023-06-03 16:36:28 +0600

    add minimax algo

--- composables/game.ts
+++ composables/game.ts
@@ -25,11 +25,74 @@ const next = (player: Player): Player => swap(player, Player.X, Player.O)
 const move = (index: number, player: Player, board: Board): Board =>
   board[index] || isFull(board) ? board : setByIndex(index, player, board)
 
+const minimax = (board: Board, depth: number, isMaximizingPlayer: boolean): number => {
+  if (check(board, Player.X)) return -1
+
+  if (check(board, Player.O)) return 1
+
+  if (isFull(board)) return 0
+
+  if (isMaximizingPlayer) {
+    let bestScore = Number.NEGATIVE_INFINITY
+
+    for (let i = 0; i < board.length; i++) {
+      if (board[i] === null) {
+        board[i] = Player.O
+        const score = minimax(board, depth + 1, false)
+        board[i] = null
+        bestScore = Math.max(score, bestScore)
+      }
+    }
+
+    return bestScore
+  } else {
+    let bestScore = Number.POSITIVE_INFINITY
+
+    for (let i = 0; i < board.length; i++) {
+      if (board[i] === null) {
+        board[i] = Player.X
+        const score = minimax(board, depth + 1, true)
+        board[i] = null
+        bestScore = Math.min(score, bestScore)
+      }
+    }
+
+    return bestScore
+  }
+}
+
+const findBestMove = (board: Board): number => {
+  let bestScore = Number.NEGATIVE_INFINITY
+  let bestMove = -1
+
+  for (let i = 0; i < board.length; i++) {
+    if (board[i] === null) {
+      board[i] = Player.O
+      const score = minimax(board, 0, false)
+      board[i] = null
+      if (score > bestScore) {
+        bestScore = score
+        bestMove = i
+      }
+    }
+  }
+
+  return bestMove
+}
+
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
+
 export const useGame = () => {
   const board = ref<Board>(createBoard())
   const player = ref<Player>(randomPlayer())
   const winLine = ref<Line | null>(null)
   const finished = computed(() => isFull(board.value) || !!winLine.value)
+  const makeMove = (index: number) => {
+    if (finished.value) return
+    player.value = next(player.value)
+    board.value = move(index, player.value, board.value)
+    winLine.value = check(board.value, player.value)
+  }
 
   return {
     finished,
@@ -41,11 +104,12 @@ export const useGame = () => {
       player.value = randomPlayer()
       winLine.value = null
     },
-    move: (index: number) => {
-      if (finished.value) return
-      player.value = next(player.value)
-      board.value = move(index, player.value, board.value)
-      winLine.value = check(board.value, player.value)
+    move: async (index: number) => {
+      makeMove(index)
+      await sleep(600)
+      if (!finished.value) {
+        makeMove(findBestMove(board.value))
+      }
     },
   }
 }
